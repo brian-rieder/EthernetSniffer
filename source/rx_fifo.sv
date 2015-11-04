bff995834aa0f78977ceed24a68b51c9ccd5709b
Brian Rieder <brieder@purdue.edu>
2015-11-04 18:50:30 -0500

    Added FIFO wrapper skeleton

---
+++ source/rx_fifo.sv
@@ -0,0 +1,14 @@
+// File name:   rx_fifo.sv
+// Created:     6 October 2015
+// Author:      Brian Rieder
+// Description: FIFO Block
+
+module rx_fifo
+(
+  // port declaration
+);
+
+fifo RX_FIFO (.r_clk(), .w_clk(), .n_rst(), .r_enable(), 
+              .w_enable(), .w_data(), .r_data(), .empty(), .full());
+
+endmodule
