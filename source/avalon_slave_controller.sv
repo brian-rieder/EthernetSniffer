d70c0f3bb5103a2aa9080da2949763fdd4eaaf35
Brian Rieder <brieder@purdue.edu>
2015-11-05 16:46:45 -0500

    Added ASC module

---
+++ source/avalon_slave_controller.sv
@@ -0,0 +1,16 @@
+// File name:   avalon_slave_controller.sv
+// Created:     05 November 2015
+// Authors:     Brian Rieder 
+//              Catie Cowden 
+//              Shaughan Gladden
+// Description: avalon_slave_controller
+
+module avalon_slave_controller
+#(
+  // parameter declaration
+)
+(
+  // port declaration
+);
+
+endmodule
