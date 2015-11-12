996674250366c4827335587a52153a3a205d430a
Brian Rieder <brieder@purdue.edu>
2015-11-12 14:58:35 -0500

    Added states and inputs

--- source/controller.sv
+++ source/controller.sv
@@ -3,14 +3,27 @@
 // Authors:     Brian Rieder 
 //              Catie Cowden 
 //              Shaughan Gladden
-// Description: controller
+// Description: Controller state machine -- determines the outputs and 
+//              functionality of the system by outputting signals to indicate
+//              readiness of subsystems.
 
 module controller
-#(
-  // parameter declaration
-)
 (
   // port declaration
+  input wire clk,
+  input wire [32:0] data_in,
+  input wire match,
+  input wire shift_enable,
+  output wire rdreq,
+  output wire read,
+  output wire inc_addr,
+  output wire addr
 );
+
+typedef enum logic [3:0] {
+  RESET, LOAD_COMP_REG, IDLE, LOAD_INPUT_FIFO, LOAD_COMPARATORS, COMPARE_CONTENTS, MATCH_FOUND, LOAD_MEMORY, ERROR  
+} state_type;
+
+state_type state, nextstate;
 
 endmodule
