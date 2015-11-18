2901f342565a2f68c795dae184d903d6dd85ea17
Brian Rieder <brieder@purdue.edu>
2015-11-17 21:26:05 -0500

    Added the actual file

--- source/ip_comparator.sv
+++ source/ip_comparator.sv
@@ -10,19 +10,21 @@ module ip_comparator
   // port declaration
   input wire clk,
   input wire n_rst,
+  input reg [31:0] ip_in,
   input reg [31:0] data_in,
   output reg [31:0] data_out
 );
 
-reg [31:0] a1, a2, a3, a4, a5; // four byte comparator buffers
+reg [31:0] a1, a2; // four byte comparator buffers
+
+// always_comb begin
+
+// end
 
 always_ff @ (posedge clk, negedge n_rst) begin
   if (n_rst == 0) begin
     a1 <= '0;
     a2 <= '0;
-    a3 <= '0;
-    a4 <= '0;
-    a5 <= '0;
     data_out <= '0;
   end else begin
     a1 <= data_in;
