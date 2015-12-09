f71faf2682076137197484b7c9bf9c5af86f4b70
Brian Rieder <brian.rieder777@gmail.com>
2015-12-08 19:41:48 -0500

    Added basic copying script to move files from Quartus/Qsys directory to project

---
+++ fpga_files/update_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# NOTE: This is Brian's script. It won't work for anyone else. That means you, Catie, stop snooping.
+
+q_dir="/cygdrive/c/Users/Brian/QuartusQsys/demo_slave_only"
+included_files=( "$q_dir/master_example.sv" "$q_dir/custom_slave.sv" "$q_dir/linux_app_sample/app.c" )
+
+for f in "${included_files[@]}" ; do
+   cp $f .
+done
