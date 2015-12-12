e37535b11d87fd239ad620014ae16c8bf88af35f
Caitlin Cowden <ccowden@purdue.edu>
2015-12-12 11:08:35 -0500

    Cleaning up test benches

--- source/tb_ethernetsniffer.sv
+++ source/tb_ethernetsniffer.sv
@@ -1,9 +1,9 @@
-// File name:   tb_mac_comparator.sv
+// File name:   tb_ethernetsniffer.sv
 // Updated:     30 November 2015
 // Authors:     Brian Rieder 
 //              Catie Cowden 
 //              Shaughan Gladden
-// Description: Test Bench for the String comparator.
+// Description: Test bench for the top level  module.
 
 `timescale 1ns/10ps
 
@@ -83,7 +83,7 @@ endclocking
 
 initial
 begin
-	//Initializations
+	// Initializations
 	sample_data = 32'h0000;
         sample_data_2 = 32'h0000;
 	sample_data_3 = 32'h0000;
@@ -101,13 +101,14 @@ begin
 	expected_addr_out = 1'b0;
 	expected_wr_en = 1'b0;
 
+	// "User programmed" test data
 	strlen = 13;
 	flagged_string = "www.wired.com";
 	flagged_ip = 32'h80D207C8; //128.210.7.200
 	flagged_port = 16'h0050; //Port 80
 	flagged_mac = 48'h641225eb1080; 
 
-	//Reset Test Case
+	//*******************Test Case 0: Reset Test Case *********************//
 	cb.n_rst <= 1'b1;
 	@cb; cb.n_rst <= 1'b0; @cb; cb.n_rst <= 1'b1;
 	assert(expected_data_out == cb.datao)
@@ -122,12 +123,16 @@ begin
 	live_data = 
 6248'h641225eb1080809b203d14740800450002fb9e6b4000400600410aba05fd80d207c8cc89005066fd81a57610e49e80187210a46800000101080a01091fd77893c23f474554202f20485454502f312e310d0a486f73743a207777772e7075726475652e6564750d0a557365722d4167656e743a204d6f7a696c6c612f352e3020285831313b205562756e74753b204c696e7578207838365f36343b2072763a34312e3029204765636b6f2f32303130303130312046697265666f782f34312e300d0a4163636570743a20746578742f68746d6c2c6170706c69636174696f6e2f7868746d6c2b786d6c2c6170706c69636174696f6e2f786d6c3b713d302e392c2a2f2a3b713d302e380d0a4163636570742d4c616e67756167653a20656e2d55532c656e3b713d302e350d0a4163636570742d456e636f64696e673a20677a69702c206465666c6174650d0a436f6f6b69653a205f5f75746d613d3134313232383333392e3634383636353639382e313433313836393238322e313434353839303937392e313434353930393839302e333b205f5f75746d7a3d3134313232383333392e313433313836393238322e312e312e75746d6373723d676f6f676c657c75746d63636e3d286f7267616e6963297c75746d636d643d6f7267616e69637c75746d6374723d286e6f7425323070726f7669646564293b205f67613d4741312e322e3634383636353639382e313433313836393238323b206b6d5f75713d3b206b6d5f6c763d783b206b6d5f61693d323635353835363b206b6d5f6e693d323635353835363b2042494769705365727665727e5745427e706f6f6c5f6c707077656261706130312e697461702e7075726475652e6564755f7765623d333033363637383636362e302e303030300d0a436f6e6e656374696f6e3a206b6565702d616c6976650d0a49662d4d6f6469666965642d53696e63653a204d6f6e2c203233204e6f7620323031352032313a31353a343120474d540d0a49662d4e6f6e652d4d617463683a20223837303066322d343962392d35323533626261333662393430220d0a0d0a;
 	
+	// When update_done is pulsed, a new packet may be read in as soon as an sop signal is received.
 	@cb; update_done = 1'b1;
 	@cb; update_done = 1'b0; ready = 1'b1; valid = 1'b1; sop = 1'b1;
 	@cb; sop = 1'b0;
 	@cb; @cb; 
-	@cb; @cb; empty = 2'b00; @cb;
+	@cb; @cb; 
+	empty = 2'b00; // There is data in the FIFO to be read.
+	@cb;
 
+	// Simulate input data operation. 4 bytes are read at a time beginning at the highest ordered data.
 	for (i = 6248-32; i > 128; i = i - 32) begin
 		sample_data   = live_data [i     +: 32];
 		sample_data_2 = live_data [i-32  +: 32];
@@ -137,9 +142,11 @@ begin
 		compare(sample_data, sample_data_2, sample_data_3, sample_data_4, sample_data_5);
 	end
 
+	// Eop is asserted at the end of a packet. The FIFO is now empty.
 	eop = 1'b1; empty = 2'b01;
 	@cb; eop = 1'b0;
 
+	// Verify that, prior to checking the match flags, all hit totals are zero.
 	expected_mac_hits = 0; expected_ip_hits = 0; expected_url_hits = 0; expected_port_hits = 0;
 	assert(expected_mac_hits == cb.mhits)
 	else $error("2: Incorrect mac match count.");
@@ -149,6 +156,7 @@ begin
 	else $error("2: Incorrect url match count.");
 	assert(expected_port_hits == cb.phits)
 	else $error("2: Incorrect port match count.");
+
 
 	@cb; @cb; @cb; @cb; @cb; @cb; @cb; @cb; @cb;
 
@@ -174,6 +182,7 @@ begin
 	eop = 1'b1; empty = 2'b01;
 	@cb; eop = 1'b0;
 
+	// Verify that, prior to checking the match flags, the ip, port, and mac were identified as hits.
 	expected_mac_hits = 1; expected_ip_hits = 1; expected_url_hits = 0; expected_port_hits = 1;
 	assert(expected_mac_hits == cb.mhits)
 	else $error("3: Incorrect mac match count.");
@@ -208,6 +217,7 @@ live_data3 =
 	eop = 1'b1; empty = 2'b01;
 	@cb; eop = 1'b0;
 
+	// Verify that, prior to checking the match flags, the url, port, and mac hits have increased by one.
 	expected_mac_hits = 2; expected_ip_hits = 1; expected_url_hits = 1; expected_port_hits = 2;
 	assert(expected_mac_hits == cb.mhits)
 	else $error("4: Incorrect mac match count.");
@@ -220,6 +230,44 @@ live_data3 =
 
 	@cb; @cb; @cb; @cb; @cb; @cb; @cb; @cb; @cb;
 
+	//*******************Test Case 4: www.extremetech.com with ERROR *********************//
+live_data3 =
+5352'h641225eb1080809b203d147408004500028e4e3040004006bd200aba0eac1700fcb3db71005034659269b1f7b37d801800e533cf00000101080a018edc497d660bdd474554202f20485454502f312e310d0a486f73743a207777772e65787472656d65746563682e636f6d0d0a557365722d4167656e743a204d6f7a696c6c612f352e3020285831313b205562756e74753b204c696e7578207838365f36343b2072763a34312e3029204765636b6f2f32303130303130312046697265666f782f34312e300d0a4163636570743a20746578742f68746d6c2c6170706c69636174696f6e2f7868746d6c2b786d6c2c6170706c69636174696f6e2f786d6c3b713d302e392c2a2f2a3b713d302e380d0a4163636570742d4c616e67756167653a20656e2d55532c656e3b713d302e350d0a4163636570742d456e636f64696e673a20677a69702c206465666c6174650d0a436f6f6b69653a205f63625f6c733d313b205f5f75746d613d3130363831303936372e3935313333303339352e313434373631373230372e313434373631373230372e313434373631373230372e313b205f5f75746d7a3d3130363831303936372e313434373631373230372e312e312e75746d6373723d28646972656374297c75746d63636e3d28646972656374297c75746d636d643d286e6f6e65293b207a6462623d3874544c694835355379796647553341315a677671673b20685f7a6462623d66326434636238383765373934623263396631393464633064353938326661613b205f5f61747576633d3625374334363b205f636861727462656174323d535a33674677345455506d7539344f2e313434373631373232333636322e313434373631373538333332372e310d0a436f6e6e656374696f6e3a206b6565702d616c6976650d0a0d0a00;
+	
+	@cb; update_done = 1'b1;
+	@cb; update_done = 1'b0; ready = 1'b1; valid = 1'b1; sop = 1'b1;
+	@cb; sop = 1'b0;
+	@cb; @cb; 
+	@cb; @cb; empty = 2'b00; @cb;
+
+	// Send approximatey half of the live_data3 data.
+	for (i = 5352-32; i > 2016; i = i - 32) begin
+		sample_data   = live_data3 [i     +: 32];
+		sample_data_2 = live_data3 [i-32  +: 32];
+		sample_data_3 = live_data3 [i-64  +: 32];
+		sample_data_4 = live_data3 [i-96  +: 32];
+		sample_data_5 = live_data3 [i-128 +: 32];
+		compare(sample_data, sample_data_2, sample_data_3, sample_data_4, sample_data_5);
+	end
+
+	// Simulate an error.
+	error = '1; valid = '0;
+
+	// Continue sending the remainder of the packet.
+	for (i = 2016-32; i > 128; i = i - 32) begin
+		sample_data   = live_data3 [i     +: 32];
+		sample_data_2 = live_data3 [i-32  +: 32];
+		sample_data_3 = live_data3 [i-64  +: 32];
+		sample_data_4 = live_data3 [i-96  +: 32];
+		sample_data_5 = live_data3 [i-128 +: 32];
+		compare(sample_data, sample_data_2, sample_data_3, sample_data_4, sample_data_5);
+	end
+
+	// Eop assertion while the error flag is high should send the controller back to the IDLE state.
+	eop = 1'b1; empty = 2'b01;
+	@cb; eop = 1'b0;
+
+	// Verify that hit counts did not change due to invalid packet.
 	expected_mac_hits = 3; expected_ip_hits = 1; expected_url_hits = 1; expected_port_hits = 3;
 	assert(expected_mac_hits == cb.mhits)
 	else $error("4: Incorrect mac match count.");
@@ -230,7 +278,7 @@ live_data3 =
 	assert(expected_port_hits == cb.phits)
 	else $error("4: Incorrect port match count.");
 
-
+	error = '0; valid = '1;
 $stop;
 
 end
@@ -244,12 +292,12 @@ task compare;
 	begin
 	data_in = sample_data; 
 	expected_data_out = 32'h0000;
+	
+	@cb; data_in = sample_data_2;
+	@cb; data_in = sample_data_3; 
+	@cb; data_in = sample_data_4; 
+	@cb; data_in = sample_data_5; 
 	@cb;
-
-	data_in = sample_data_2; @cb;
-	data_in = sample_data_3; @cb;
-	data_in = sample_data_4; @cb;
-	data_in = sample_data_5; @cb;
 
 	// DATA OUT
 	expected_data_out = sample_data;
@@ -275,8 +323,7 @@ task compare;
 	assert(expected_data_out == cb.datao)
 	else $error("2.6: Incorrect Data_Out");
 	
-	@cb; @cb; @cb; @cb;
-	@cb;
+	@cb; @cb; @cb; @cb; @cb;
 	end	
 endtask
 
--- source/tb_ip_comparator.sv
+++ source/tb_ip_comparator.sv
@@ -44,30 +44,31 @@ end
 clocking cb @(posedge clk);
 	default input #1step output #100ps;
 	output #800ps n_rst = n_rst;
-	output datai = data_in;
-	output flaggedip = flagged_ip;
-	output c = clear;
-	input datao = data_out;
-	input m = match;
+	output datai        = data_in;
+	output flaggedip    = flagged_ip;
+	output c            = clear;
+	input datao         = data_out;
+	input m             = match;
 endclocking
 
 initial
 begin
-	n_rst = 1'b1;
-  sample_data = 32'hC0A80101; //192.168.001.001
-	sample_data_shift1 = 32'h01000000;
+	n_rst                = 1'b1;
+  	sample_data          = 32'hC0A80101; //192.168.001.001
+	sample_data_shift1   = 32'h01000000;
 	sample_data_shift1_2 = 32'h00C0A801;
-	sample_data_shift2 = 32'h01010000;
+	sample_data_shift2   = 32'h01010000;
 	sample_data_shift2_2 = 32'h0000C0A8;
-	sample_data_shift3 = 32'hA8010100;
+	sample_data_shift3   = 32'hA8010100;
 	sample_data_shift3_2 = 32'h000000C0;
-	data_in = 32'h0000;
-	flagged_ip = 32'h0000;
-	clear = '0;
+	data_in              = '0;
+	flagged_ip           = 32'h0000;
+	clear                = '0;
 
-	//Reset Test Case
+	//*******************Test Case 1: Reset*********************//
 	cb.n_rst <= 1'b1;
-	@cb; n_rst = 1'b0; @cb;
+	@cb; n_rst = 1'b0; 
+	@cb;
 	expected_data_out = '0;
 	expected_match = '0;
 	assert(expected_data_out == cb.datao)
@@ -75,199 +76,195 @@ begin
 	assert(expected_match == cb.m)
 	else $error("1: Reset Test Case: Incorrect Match Flag");
 	cb.n_rst <= 1'b1;
-	@cb; @cb;
-	
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; @cb; @cb;
 
-	//*******************Test Case 2.1 No Shift*********************//
+	clear = 1'b1; 
+	@cb; clear = 1'b0;
+
+	//*******************Test Case 2: No Shift*********************//
 	flagged_ip = sample_data; 	
 	data_in = sample_data; 
-	@cb; //@cb;
+	@cb;
 
-	//Test Case 2.2
-	data_in = 32'h0000; 
-	@cb; //@cb;
-	expected_match = '0;
+	data_in = '0; 
+	@cb; expected_match = '0;
 	assert(expected_match == cb.m)
-	else $error("2.1: successful match: incorrect match flag");
+	else $error("2.1: No shift: incorrect match flag");
 
 	@cb; @cb; 
 	expected_match = 1'b1;
 	assert(expected_match == cb.m)
-	else $error("2.2: Successful Match: Incorrect Match Flag");
+	else $error("2.2: No shift: Incorrect Match Flag");
 	expected_data_out = sample_data;
 	assert(expected_data_out == cb.datao)
-	else $error("2.1: Successful Match: Incorrect Data_Out");
+	else $error("2.1: No shift: Incorrect Data_Out");
 
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("2.2: Successful Match: Incorrect Data_Out");
+	else $error("2.2: No shift: Incorrect Data_Out");
 	@cb; clear = 1'b1; @cb; clear = 1'b0;
 
-	//*******************Test Case 3.1: One Shift*********************//
+	//*******************Test Case 3: One Shift*********************//
 	data_in = sample_data_shift1_2;
 	expected_match = '0;
 	@cb;
 	assert(expected_match == cb.m)
-	else $error("3.1: Successful Match: Incorrect Match Flag");
+	else $error("3.1: One Shift: Incorrect Match Flag");
 
-	//Test Case 3.2
 	data_in = sample_data_shift1;
 	expected_match = 1'b0;
 	@cb;
 	assert(expected_match == cb.m)
-	else $error("3.2: Successful Match: Incorrect Match Flag");
+	else $error("3.2: One Shift: Incorrect Match Flag");
 	
-	//Test Case 3.3
-	data_in = 32'h0000;
+	data_in = '0;
 	@cb;
 	expected_data_out = sample_data_shift1_2;
 	expected_match = 1'b1;
 	@cb;
 	assert(expected_data_out == cb.datao)
-	else $error("3.1: Successful Match: Incorrect Data_Out");
+	else $error("3.1: One Shift: Incorrect Data_Out");
 	assert(expected_match == cb.m)
-	else $error("3.3: Successful Match: Incorrect Match Flag");
-	
+	else $error("3.3: One Shift: Incorrect Match Flag");
 
 	@cb; 
 	expected_data_out = sample_data_shift1;
 	assert(expected_data_out == cb.datao)
-	else $error("3.2: Successful Match: Incorrect Data_Out");
+	else $error("3.2: One Shift: Incorrect Data_Out");
 	
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("3.3: Successful Match: Incorrect Data_Out");
+	else $error("3.3: One Shift: Incorrect Data_Out");
 
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; clear = 1'b1; 
+	@cb; clear = 1'b0;
 
-	//*******************Test Case 4.1: Two Shift*********************//
+	//*******************Test Case 4: Two Shifts*********************//
 	data_in = sample_data_shift2_2;
 	expected_match = '0;
 	@cb; 
 	assert(expected_match == cb.m)
-	else $error("4.1: Successful Match: Incorrect Match Flag");
+	else $error("4.1: Two Shifts: Incorrect Match Flag");
 
 	//Test Case 4.2
 	data_in = sample_data_shift2;
 	expected_match = 1'b0;
-	@cb; //@cb;
+	@cb;
 	assert(expected_match == cb.m)
-	else $error("4.2: Successful Match: Incorrect Match Flag");
+	else $error("4.2: Two Shifts: Incorrect Match Flag");
 	
 	//Test Case 4.3
-	data_in = 32'h0000;
+	data_in = '0;
 	expected_match = 1'b1;
-	@cb; //@cb;
+	@cb;
 	expected_data_out = sample_data_shift2_2;
 	@cb;
 	assert(expected_data_out == cb.datao)
-	else $error("4.1: Successful Match: Incorrect Data_Out");
+	else $error("4.1: Two Shifts: Incorrect Data_Out");
 	assert(expected_match == cb.m)
-	else $error("4.3: Successful Match: Incorrect Match Flag");
+	else $error("4.3: Two Shifts: Incorrect Match Flag");
 	
 	@cb; expected_data_out = sample_data_shift2;
 	assert(expected_data_out == cb.datao)
-	else $error("4.2: Successful Match: Incorrect Data_Out");
+	else $error("4.2: Two Shifts: Incorrect Data_Out");
 
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("4.3: Successful Match: Incorrect Data_Out");
+	else $error("4.3: Two Shifts: Incorrect Data_Out");
 
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; clear = 1'b1;
+	@cb; clear = 1'b0;
 
-	//*******************Test Case 5.1: Two Shift*********************//
+	//*******************Test Case 5.1: Three Shifts*********************//
 	data_in = sample_data_shift3_2;
 	expected_match = '0;
 	@cb;
 	assert(expected_match == cb.m)
-	else $error("5.1: Successful Match: Incorrect Match Flag");
+	else $error("5.1: Three Shifts: Incorrect Match Flag");
 
 	//Test Case 5.2
 	data_in = sample_data_shift3;
 	expected_match = 1'b0;
 	@cb;
 	assert(expected_match == cb.m)
-	else $error("5.2: Successful Match: Incorrect Match Flag");
+	else $error("5.2: Three Shifts: Incorrect Match Flag");
 	
 	//Test Case 5.3
-	data_in = 32'h0000;
+	data_in = '0;
 	expected_match = 1'b1;
-	@cb;
-	expected_data_out = sample_data_shift3_2;
+	@cb; expected_data_out = sample_data_shift3_2;
 	@cb;
 	assert(expected_data_out == cb.datao)
-	else $error("5.1: Successful Match: Incorrect Data_Out");
+	else $error("5.1: Three Shifts: Incorrect Data_Out");
 	assert(expected_match == cb.m)
-	else $error("5.3: Successful Match: Incorrect Match Flag");
+	else $error("5.3: Three Shifts: Incorrect Match Flag");
 	
 	@cb; expected_data_out = sample_data_shift3;
 	assert(expected_data_out == cb.datao)
-	else $error("5.2: Successful Match: Incorrect Data_Out");
+	else $error("5.2: Three Shifts: Incorrect Data_Out");
 	
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("5.3: Successful Match: Incorrect Data_Out");
+	else $error("5.3: Three Shifts: Incorrect Data_Out");
 
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; clear = 1'b1;
+	@cb; clear = 1'b0;
 
-//*******************Test Case 6.1: All Ones*********************//
+//*******************Test Case 6: All Ones*********************//
 	flagged_ip = '1;
 	data_in = '1;
 	expected_match = 1'b0;
-	@cb;
 
 	//Test Case 6.2
-	expected_match = 1'b0;
-	@cb;
+	@cb; expected_match = 1'b0;
 	
 	//Test Case 6.3
-	data_in = 32'h0000;
+	@cb; data_in = '0;
 	expected_match = 1'b1;
 	@cb; expected_data_out = '1; @cb;
 	assert(expected_data_out == cb.datao)
-	else $error("6.1: Successful Match: Incorrect Data_Out");
+	else $error("6.1: All Ones: Incorrect Data_Out");
 	assert(expected_match == cb.m)
-	else $error("6.3: Successful Match: Incorrect Match Flag");
+	else $error("6.3: All Ones: Incorrect Match Flag");
 	
 	@cb;
 	assert(expected_data_out == cb.datao)
-	else $error("6.2: Successful Match: Incorrect Data_Out");
+	else $error("6.2: All Ones: Incorrect Data_Out");
 	
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("6.3: Successful Match: Incorrect Data_Out");
+	else $error("6.3: All Ones: Incorrect Data_Out");
 
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; clear = 1'b1;
+	@cb; clear = 1'b0;
 
 //*******************Test Case 7.1: All Zeros*********************//
 	flagged_ip = '0;
 	data_in = '0;
 	expected_match = 1'b0;
-	@cb;
 
 	//Test Case 7.2
-	expected_match = 1'b0;
-	@cb;
+	@cb; expected_match = 1'b0;
 	
 	//Test Case 7.3
-	data_in = 32'h0000;
+	@cb; data_in = '0;
 	expected_match = 1'b1;
 	@cb; expected_data_out = '0; @cb;
 	assert(expected_data_out == cb.datao)
-	else $error("7.1: Successful Match: Incorrect Data_Out");
+	else $error("7.1: All Zeros: Incorrect Data_Out");
 	assert(expected_match == cb.m)
-	else $error("7.3: Successful Match: Incorrect Match Flag");
+	else $error("7.3: All Zeros: Incorrect Match Flag");
 	
 	@cb;
 	assert(expected_data_out == cb.datao)
-	else $error("7.2: Successful Match: Incorrect Data_Out");
+	else $error("7.2: All Zeros: Incorrect Data_Out");
 	
-	@cb; expected_data_out = 32'h0000;
+	@cb; expected_data_out = '0;
 	assert(expected_data_out == cb.datao)
-	else $error("7.3: Successful Match: Incorrect Data_Out");
+	else $error("7.3: All Zeros: Incorrect Data_Out");
 
-	@cb; clear = 1'b1; @cb; clear = 1'b0;
+	@cb; clear = 1'b1;
+	@cb; clear = 1'b0;
 
 	$stop;
 
